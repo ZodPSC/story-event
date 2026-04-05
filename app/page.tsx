a65d9bcde3fa85186853a30959e73f559a555863
Dmitriy <d>
2026-04-05 17:46:49 +0300

    edit home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,20 @@
 import Image from "next/image";
+import { FC } from "react";
 
+const Home: FC = () => {
+  return (
+      <div>
+        <h1>Добро пожаловать на главную страницу</h1>
+      </div>
+  )
+}
+
+export default Home;
+
+/*
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <Image
           className="dark:invert"
@@ -63,3 +75,4 @@ export default function Home() {
     </div>
   );
 }
+*/
